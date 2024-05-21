4a4e869a3c192b08e5f601dacb09ccff65224e76
Suguru Miyata <suguru1114@gmail.com>
2024-05-21 13:11:55 +1000

    small changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
-import {useState} from "react";
-// import { Routes, Route, useNavigate } from 'react-router-dom';
-
-import  Calendar from "react-calendar";
-// import DatePage from './DatePage';
+import Calendar from 'react-calendar';
 
 function App() {
-
   const [date, setDate] = useState(new Date());
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+    navigate(`/date/${newDate.toISOString().split('T')[0]}`);
+  };
 
   return (
     <div className="App">
-
       <h1 className='text-center'>Calendar</h1>
-          <div className='calendar-container'>
-            <Calendar onChange={setDate} value={date} />
-          </div>
-          <p className='text-center'>
-            <span className='bold'>Selected Date:</span>{' '}
-            {date.toDateString()}
-          </p>
+      <div className='calendar-container'>
+        <Calendar onChange={handleDateChange} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
     </div>
   );
 }
